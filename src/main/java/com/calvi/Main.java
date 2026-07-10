8a9b857923cd85c2a14d7b15ad2215bcbf8e7b17
ImSayvi <say.julia.vi@gmail.com>
2026-07-10 22:43:21 +0200

    first layout sketch

--- src/main/java/com/calvi/Main.java
+++ src/main/java/com/calvi/Main.java
@@ -4,8 +4,10 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
+import javafx.scene.layout.Priority;
 
 public class Main extends Application{
     public static void main(String[] args) {
@@ -15,17 +17,48 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage){
-        BorderPane root = new StackPane();
-        Scene scene = new Scene(root);
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 1200, 800);
 
         stage.setScene(scene);
-        Label label = new Label("test javaFX");
-        label.setFont(new Font("Arial", 25));
+
+        Label monthsLabel  = new Label("Widok miesięcy");
+        Label weekLabel = new Label("Widok tygodnia");
+        Label notesLabel  = new Label("notatki");
+        Label dayLabel  = new Label("widok dnia");
+        monthsLabel.setFont(new Font("Arial", 25));
+        weekLabel.setFont(new Font("Arial", 25));
+        notesLabel.setFont(new Font("Arial", 25));
+        dayLabel.setFont(new Font("Arial", 25));
         
-        root.getChildren().add(label);
+        StackPane monthsPane = new StackPane();
+        monthsPane.setStyle("-fx-background-color: blue;");
+        monthsPane.getChildren().add(monthsLabel);
 
+        StackPane weekPane = new StackPane();
+        weekPane.setStyle("-fx-background-color: red;");
+        weekPane.getChildren().add(weekLabel);
 
-        stage.setTitle("test java FX");
+        StackPane notesPane = new StackPane(); 
+        notesPane.setPrefWidth(250);
+        notesPane.setStyle("-fx-background-color: pink;"); 
+        notesPane.getChildren().add(notesLabel);
+
+        StackPane dayPane = new StackPane();
+        dayPane.setPrefWidth(250);
+        dayPane.setStyle("-fx-background-color: yellow;"); 
+        dayPane.getChildren().add(dayLabel);
+
+        VBox center = new VBox();
+        center.getChildren().addAll(monthsPane, weekPane);
+        VBox.setVgrow(monthsPane, Priority.ALWAYS);
+        VBox.setVgrow(weekPane, Priority.ALWAYS);
+
+        root.setLeft(notesPane);
+        root.setRight(dayPane);
+        root.setCenter(center);
+
+        stage.setTitle("CalVi");
         stage.show();
     }
 }
